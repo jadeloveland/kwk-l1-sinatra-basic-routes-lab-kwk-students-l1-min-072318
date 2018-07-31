af067aa9b658604570cda5d2d913ad5a64850df7
jadeloveland <jloveland93@gmail.com>
2018-07-31 16:13:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get'/' do
+    "Hello, World!"
+  end
 end
+
+class App < Sinatra::Base
+  get'/name'do
+    "My name is Jade"
+end
+
+class App < Sinatra::Base
+  get'/himetown'
+  "My hometown is Victoria"
+end
+
+class App < Sinatra::Base 
+  get'favorite-song
